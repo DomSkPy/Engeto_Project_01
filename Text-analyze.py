6e408977c28f3474501e5f2451021faf713bc4db
DomSkPy <python.skrenekD@gmail.com>
2022-07-14 15:32:16 +0200

    Engeto_project_01

--- Text-analyze.py
+++ Text-analyze.py
@@ -20,47 +20,22 @@ discord: DomSk
 '''
 author = Dominik Skrenek
 '''
+from task import TEXTS
 
-TEXTS = ['''
-Situated about 10 miles west of Kemmerer,
-Fossil Butte is a ruggedly impressive
-topographic feature that rises sharply
-some 1000 feet above Twin Creek Valley
-to an elevation of more than 7500 feet
-above sea level. The butte is located just
-north of US 30N and the Union Pacific Railroad,
-which traverse the valley. ''',
-'''At the base of Fossil Butte are the bright
-red, purple, yellow and gray beds of the Wasatch
-Formation. Eroded portions of these horizontal
-beds slope gradually upward from the valley floor
-and steepen abruptly. Overlying them and extending
-to the top of the butte are the much steeper
-buff-to-white beds of the Green River Formation,
-which are about 300 feet thick.''',
-'''The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.'''
-]
-
-#Welcoming user
-print("Welcome to text analyzer!")
-login_name = input(f"Please entry your login name:")
-login_password = input(f"Please entry your password:")
-line = ("-" * 40)
-print(line)
-print(f"username: {login_name}")
-print(f"password: {login_password}")
-print(line)
+#Registery users
 registry_users = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"}
 name = registry_users.keys
-registry = registry_users.values
+line = ("-" * 40)
+#Welcoming user
+print(line)
+print((" " * 3) + ("Welcome to text analyzer!"))
+print(line)
+login_name = input(f"Please entry your login name:")
+login_password = input(f"Please entry your password:")
+print(line)
+print(f"username: {login_name}")
+print(line)
+
 
 #entry for user
 if registry_users.get(login_name) == login_password:
@@ -74,15 +49,15 @@ print(line)
 number_of_text = input(f"Enter a number btw. 1 and {len(TEXTS)}:")
 print(line)
 
-if not number_of_text.isnumeric():
+if not number_of_text.isdigit():
     print(f"Can't allocate.")
     exit()
 elif int(number_of_text) == 1:
-    text_ch = str(TEXTS[0])
+    text_ch = (TEXTS[0])
 elif int(number_of_text) == 2:
-    text_ch = str(TEXTS[1])
+    text_ch = (TEXTS[1])
 elif int(number_of_text) == 3:
-    text_ch = str(TEXTS[2])
+    text_ch = (TEXTS[2])
 else:
     print("Wrong number.")
     exit()
@@ -103,12 +78,13 @@ for w in text_ch.split():
         frec[len(w)] = 1
     else:
         frec[len(w)] += 1
+
     if w.istitle():
-        title_case = title_case + 1
+        title_case += w.istitle()
     elif w.isupper():
-        upper_case = upper_case + 1
+        upper_case += w.isupper()
     elif w.islower():
-        lower_case = lower_case + 1
+        lower_case += w.islower()
     elif w.isdigit():
         digits.append(int(w))
 
@@ -123,6 +99,13 @@ print(
 print(line)
 
 # Graf
+print(line)
+zah = "|Width|" + (" " * 2) + "|Occurences|" + (" " * 3) + "|Numb.|" + (" " * 3)
+print(zah)
+print(line)
+
+
 for a, b in frec.items():
     print(a, int(b) * "*", b)
 print(line)
+
